f64c2ae9a704340900cdc8e4dbd654d17302ed78
Quan Truong <qmtruong92@gmail.com>
2017-01-16 03:38:40 -0700

    Exercise 4-1 of ThinkJava, 2016

--- Chapter4/src/WuggaWug.java
+++ Chapter4/src/WuggaWug.java
@@ -1,3 +1,14 @@
+/**
+ * Exercise 4-1 of ThinkJava, 2016.
+ * 
+ * What is the output of the following program?
+ * 
+ * HINT: You can use breakpoints to walk you through each step of the code (if using an IDE like eclipse)
+ * 
+ * @author Quan Truong
+ *
+ */
+
 public class WuggaWug {
 
     public static void zoop() {
