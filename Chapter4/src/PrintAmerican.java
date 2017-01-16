4e15f1855b332f3644e73251ad7885f3b010b7c7
Quan Truong <qmtruong92@gmail.com>
2017-01-16 03:52:51 -0700

    Exercise 4-2 of Think Java, 2016.

--- Chapter4/src/PrintAmerican.java
+++ Chapter4/src/PrintAmerican.java
@@ -1,30 +1,53 @@
-
+/**
+ * Exercise 4-3 of Think Java, 2016.
+ * 
+ * Encapsulate the exercise from 2-2 into it's own method. The method should take 4 parameters:
+ * A String for the day, e.g. "Friday"; A integer value for the date, i.e. 12; a String for the month,
+ *  e.g. "December"; and a Integer value for the year, e.g. 1950.
+ * 
+ * @author Quan Truong
+ *
+ */
 public class PrintAmerican {
 	
-	public static void printAmerican( int date, String month, int year) {
+	/**
+	 * Print the date in American format.
+	 * @param date
+	 * 				Integer value, date of the current day, e.g. 12
+	 * @param month
+	 * 				String, the month of the year, e.g. "February".
+	 * @param year
+	 * 				Integer value, the year, e.g. 1992.
+	 */
+	public static void printAmerican(String day, int date, String month, int year) {
 		System.out.println("American Format:");
-		System.out.println(month + " " + date + ", " + year);
+		System.out.println(day + ", " + month + " " + date + ", " + year);
 		System.out.println();
 		
 	}
 	
-	public static void printEuropean( int date, String month, int year){
+	/**
+	 * Print the date in European format.
+	 * @param date
+	 * 				Integer value, date of the current day, e.g. 12
+	 * @param month
+	 * 				String, the month of the year, e.g. "February".
+	 * @param year
+	 * 				Integer value, the year, e.g. 1992.
+	 */
+	public static void printEuropean(String day, int date, String month, int year){
 		System.out.println("The date in EU format: ");
-		System.out.println((date) + " " + month + " " + year);
+		System.out.println(day + " " + date + " " + month + " " + year);
 		System.out.println();
 	}
 	
 	public static void main(String[] args) {
-		printAmerican(19, "December", 2016);
-		printEuropean(23, "Whenever", 1962);
+		printAmerican("Friday", 19, "December", 2016);
+		printEuropean("Friday", 19, "December", 2016);
 		
 	}
 	
 	
 	
-//	String day = "Thursday";
-//	String month = "July";
-//	int date = 16;
-//	int year = 2016;
 	
 }
