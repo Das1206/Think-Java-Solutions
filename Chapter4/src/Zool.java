962ac19937acbeddfc4006adffc1c42cd3b93dab
Quan Truong <qmtruong92@gmail.com>
2017-01-16 03:43:19 -0700

    Exercise 4-2 of Think Java

--- Chapter4/src/Zool.java
+++ Chapter4/src/Zool.java
@@ -1,15 +1,29 @@
-
+/**
+ * Exercise 4-2 of Think Java, 2016.
+ * 
+ * @author Quan Truong
+ *
+ */
 public class Zool {
 
-	public static void zool(int n, String s, String t, double o) {
+	/**
+	 * Write a string that takes 3 parameters: An int, and 2 Strings.
+	 * 
+	 * @param n
+	 * 			The int
+	 * @param s
+	 * 			Your first dog's name
+	 * @param t
+	 * 			The street you grew up on
+	 */
+	public static void zool(int n, String s, String t) {
 		System.out.println(n);
 		System.out.println(s);
 		System.out.println(t);
-		System.out.println(o);
 	}
 	
 	public static void main(String[] args){
-		zool(11, "yourmomma", "guesswhat", 9293);
+		zool(11, "Monty Python", "Secret Street");
 		System.err.println("This is an error");
 		
 	}
