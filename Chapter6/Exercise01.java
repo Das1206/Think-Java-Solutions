31e592534c6b3e0520030fcc2055cb8981adce09
Quan Truong <qmtruong92@gmail.com>
2017-01-16 05:44:54 -0700

    Exercise 6-1 of Think Java, 2016.

---
+++ Chapter6/Exercise01.java
@@ -0,0 +1,17 @@
+/**
+ * Exercise 6-1 of Think Java, 2016.
+ * 
+ * @author Quan Truong
+ *
+ */
+public class Exercise01 {
+
+	public static void main(String[] args) {
+		int unusedVariable; //variable is not used, compiler returns error of "local variable unusedVariable is not used"
+		
+		System.out.println("boo") + 7; //compiler returns the left hand side must be a variable
+		
+
+	}
+
+}
