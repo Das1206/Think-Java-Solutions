06d75be4296b64c82cb510003e436467538ec5a2
Quan Truong <qmtruong92@gmail.com>
2017-01-16 04:44:33 -0700

    Exercise 5-2 of Think Java, 2016.

---
+++ Chapter5/src/SingleIfStatement.java
@@ -0,0 +1,18 @@
+/**
+ * Exercise 5-2 of Think Java, 2016.
+ * 
+ * @author Quan Truong
+ *
+ */
+public class SingleIfStatement {
+
+	public static void main(String[] args) {
+		int x = 6;
+		
+		if (x > 0 && x < 10) {
+			System.out.println("positive single digit integer");
+		}
+
+	}
+
+}
