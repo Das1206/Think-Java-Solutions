4897aa1f660ec6f2205f40e604953dce8020402b
Quan Truong <qmtruong92@gmail.com>
2017-01-17 07:42:03 -0700

    Exercise 3-4 of Think Java, 2016

---
+++ Chapter3/Exercise04.java
@@ -0,0 +1,43 @@
+/**
+ * Exercise 3-4 of Think Java, 2016.
+ * A guess the number game.
+ * @author Quan Truong
+ */
+
+
+import java.util.*;
+
+public class Exercise04 {
+
+	public static void main(String[] args) {
+		int userGuess, computerValue, difference;
+		Scanner in = new Scanner(System.in);
+		
+		
+		//Generate computer value
+		Random random = new Random();
+		computerValue = random.nextInt(100) + 1;
+		
+		
+		//Ask for user input
+		System.out.println("I am thinking of a number between 1-100. Can you guess what it is? ");
+		System.out.println("Type a number: ");
+		userGuess = in.nextInt();
+
+		//Print User guess
+		System.out.println("Your guess is: " + userGuess);
+		//Print computer's value
+		System.out.println("The number I was thinking of is:  " + computerValue);
+		
+		//If the user's guess doesn't equal the computer value, print the difference and tell the user his luck ran short
+		if (userGuess != computerValue) {
+			difference = Math.abs(userGuess - computerValue);
+			System.out.println("You were off by: " + difference + ".");
+		} else if (userGuess == computerValue) {
+			System.out.println("Your guess was correct");
+			
+		}
+		
+	}
+
+}
