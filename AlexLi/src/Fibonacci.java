a667771dce7bb2ed843c8895b623ba1297b7fc34
qmtru <qmtru@DESKTOP-2P0DM82>
2017-02-08 00:00:58 -0700

    Update

--- AlexLi/src/Fibonacci.java
+++ AlexLi/src/Fibonacci.java
@@ -1,17 +1,43 @@
-
-public class FibonacciAlexLi {
+/**
+ * Finish prologue...
+ * Programmer: Quan Truong					CSC110AB <12:00 - 12:50 PM>
+ * 											
+ *
+ */
+public class Fibonacci {
 
 	public static void main(String[] args) {
-		System.out.println(fibonacci(10));
+ doExercise1();
+ 
+ doExercise2();
+ 
+ /*doExercise3();*/
+ 
+		// print first 12 numbers of the fibonacci sequence
+		for (int i = 1; i <= 12; i++) {
+			System.out.print(fibonacci(i) + " ");
+		}
+	}
 
+	public static int fibonacci(int n) {
+		if (n <= 2) {
+			return 1;
+		} else {
+			return fibonacci(n - 1) + fibonacci(n - 2);
+		}
 	}
 	
-	public static int fibonacci(int n) {
-		if (n == 0) 
-			return 0;
-		if (n==1)
-			return 1;
-		return fibonacci(n-1) + fibonacci(n-2);
+	public static void doExercise1() {
+		for (int i = 50; i >= 1; i--){
+			System.out.print(i + " ");
+		}
+		
 	}
 
+	public static void doExercise2() {
+		for (int i = 1; i <= 10; i++) {
+			double result = (int) Math.pow(i, 2);
+			System.out.println(result);
+		}
+	}
 }
