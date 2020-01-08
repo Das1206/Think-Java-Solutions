46bafe541a06eccb9326dc5d2746cabf50aec165
Das1206 <59559241+Das1206@users.noreply.github.com>
2020-01-08 11:57:20 +0200

    Update BeerBottles.java

--- Chapter4/src/Exercises/BeerBottles.java
+++ Chapter4/src/Exercises/BeerBottles.java
@@ -7,28 +7,32 @@ package Exercises;
 
 
 
-public class BeerBottles {
-	
-	
-	/**
-	 * Method below uses recursion to keep calling itself until a condition is met. In this case, the condition is n == 0.
-	 * @author Quan Truong
-	 *
-	 */
-	public static void countdown(int n) {
-		if (n == 0) {
-			System.out.println("No bottles of beer on the wall!");
-		} else {
-			System.out.println(n + "bottle of beer on the wall");
-			
-			//calls itself again here
-			countdown(n-1);
-		}
-	}
-	
-	public static void main(String[] args) {
-		countdown(100);
-	}
+
+    public static void bottles(int n){
+        //first verse only
+        if (n>0) {
+
+                if (n==1) {
+                    System.out.println(n-- + "bottle of beer");
+                } else if (n==2) {
+                System.out.println(n-- + "bottle of beer on the wall, ya' take one down, ya' pass it around, " + n + " bottle of beer on the wall, ");
+                } else {
+                    System.out.println(n-- + "bottles of beer on the wall, " + n-- + " bottles of beer, ya’ take one down, ya’ pass it around," + n + " bottles of beer on the wall.");
+                    //calls itself again here
+                }
+                bottles(n - 1);
+            }
+
+        else {
+        System.out.println("No bottles of beer on the wall, no bottles of beer, ya’ can’t take\n" +
+                "one down, ya’ can’t pass it around, ’cause there are no more\n" +
+                "bottles of beer on the wall!");
+        }
+
+    }
+    public static void main(String[] args) {
+        bottles(4);
+    }
 	
 	
 }
